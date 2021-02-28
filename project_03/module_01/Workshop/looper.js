ecbc4123ed5dde71bd858995b224ec1a853ec3ef
Erica Krebs <67703792+ekrebs1@users.noreply.github.com>
2021-02-28 17:54:53 -0600

    Delete project_03 directory

--- project_03/module_01/Workshop/looper.js
+++
@@ -1,8 +0,0 @@
-for (let index = 0; index < 12; index = index + 1) {
-  const newImage = $('<img>');
-  const newImageSrc = 'http://placeimg.com/640/480/nature?' + index;
-
-  newImage.attr('src', newImageSrc)
-
-  $('body').append(newImage)
-}
